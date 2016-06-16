545be0e5adc53ca8bc663b0fc99175db3e519f94
Mostafa Gaafar <mgaafar@live.com>
2016-06-17 01:59:59 +0400

    add module tests

--- test/index.js
+++ test/index.js
@@ -1,7 +1,44 @@
 const { assert } = require('chai');
 const deppie = require('../index');
 
+// no operation function
+const noop = () => {};
+
 describe('deppie', () => {
+    describe('dependency injection', () => {
+        it('should find the injected module', () => {
+            const dep1 = () => ({ a: 1 });
+            const dep2 = ({ dep1 }) => {
+                assert.deepEqual(dep1, { a: 1 });
+            };
+            deppie({ dep1, dep2 });
+        });
+    });
+
+    describe('existing modules', () => {
+        it('should find the existing module', () => {
+            const dep1 = { a: 1 };
+            const dep2 = ({ dep1 }) => {
+                assert.deepEqual(dep1, { a: 1 });
+            };
+            deppie({ dep2 }, { dep1 });
+        });
+    });
+
+    describe('created modules', () => {
+        const dep1 = () => ({ a: 1 });
+        const dep2 = ({ dep1 }) => {
+            noop(dep1);
+        };
+        const modules = deppie({ dep1, dep2 });
+        it('should find the created module', () => {
+            assert.deepEqual(modules.dep1, { a: 1 });
+        });
+        it('should not find the void module', () => {
+            assert.notProperty(modules, 'dep2');
+        });
+    });
+
     describe('missing dependency', () => {
         it('should throw exception for missing dependencies', () => {
             const dep1 = ({ dep2, dep3 }) => ({ a: dep2, b: dep3 });
